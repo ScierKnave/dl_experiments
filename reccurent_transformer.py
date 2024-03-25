67440343c0d4b6cb045ba18212fcc0f99e83ab30
Dereck Piché <pichedereck@gmail.com>
2024-03-25 18:06:23 -0400

    clean ups

--- reccurent_transformer.py
+++ reccurent_transformer.py
@@ -7,46 +7,51 @@ import torch
 class recurrent_transfomer(nn.Module):
     def __init__(
             self,
-            embed_size,
             nb_layers, 
             vocab_size,
+            token_size,
             hidden_length,
-            hidden_size,
-            symbolic_length,
-            symbolic_size
+            symbolic_length
     ):
         super(recurrent_transfomer, self).__init__()
-        decoder_layer = nn.TransformerDecoderLayer(d_model=embed_size, nhead=4)
+
+        # transformer 
+        decoder_layer = nn.TransformerDecoderLayer(d_model=token_size, nhead=4)
         self.transformer = nn.TransformerDecoder(decoder_layer, nb_layers)
 
-
-        self.hidden_context = torch.rand(hidden_length, hidden_size) #
+        # contexts
+        self.hidden_context = torch.rand(hidden_length, token_size) #
         self.hidden_counts = torch.zeros(hidden_length)
-        self.symbolic_context = torch.zeros(symbolic_length, symbolic_size)
+        self.symbolic_context = torch.zeros(symbolic_length, token_size)
 
-        self.symbolic_out = nn.Linear(symbolic_size*symbolic_length, vocab_size)
-        self.hidden_out = nn.Linear(symbolic_size*symbolic_length, hidden_size)
+        # dual output
+        self.symbolic_out = nn.Linear(token_size*symbolic_length, vocab_size)
+        self.hidden_out = nn.Linear(token_size*hidden_length, token_size)
 
     def autoregress(self, sequence, item, dim):
         sequence = sequence[:, 1:, :]
         sequence = torch.cat(sequence, item, dim=dim)
+        return sequence
 
     def forward(self, symbol):
+
         self.autoregress(self.symbolic_context, symbol, 1)
-        out = self.transformer(self.symbolic_context)
-        probs, hidden = (self.symbolic_out(out), self.hidden_out(out))
-        self.autoregress(self.hidden_context, hidden, 1) # add tought to sequence
+
+        pre_out = self.transformer(self.symbolic_context)
+        s_out, h_out = (self.symbolic_out(pre_out), self.hidden_out(pre_out))
+
+        self.autoregress(self.hidden_context, h_out, 1) # add tought to sequence
         self.autoregress(self.hidden_counts, torch.tensor([0]), 0)
 
         # make gradient cuts past the gradient horizons
         for pos in range(self.hidden_counts.shape[0]):
             if self.hidden_counts[pos] > self.gradient_horizon:
-                # prevent further comp graph connexions to this token
                 hidden = self.hidden_context[:, pos, :]
                 hidden = hidden.clone().detach()
                 self.self.hidden_context[:, pos, :]
 
 
-        return probs
+        # return probabilities for each vocab bit
+        return torch.softmax(s_out, dim=-1)
 
 
--- train.py
+++ train.py
@@ -6,18 +6,17 @@ import reccurent_transformer
 def train_sequence(model, optimizer, loss_function, train_tokens, epochs, step_freq):
     model.train()
     for epoch in range(epochs):
-        total_loss = 0
+
+        loss = 0
+
         for i in range(0, len(train_tokens) - 1):
-            optimizer.zero_grad()
             token = torch.tensor(train_tokens[i], dtype=torch.long).unsqueeze(0)
             next_token = torch.tensor(train_tokens[i + 1], dtype=torch.long).unsqueeze(0)
             next_token_hat = model(token)
-            loss = loss_function(next_token_hat, next_token)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
+            loss += loss_function(next_token_hat, next_token)
 
             if i % step_freq == 0:
+                loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
                 loss = 0
@@ -33,13 +32,12 @@ print(tokens[:10])
 
 # training 
 model = reccurent_transformer(
-    embed_size=512,
-    nb_layers=6,
+    nb_layers=2,
     hidden_length=10,
     hidden_size=512,
     symbolic_length=10,
     symbolic_size=512,
-    gradient_horizon=10
+    gradient_horizon=1
 )
 model = reccurent_transformer()
 optimizer = torch.optim.AdamW(model.params())
