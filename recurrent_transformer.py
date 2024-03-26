7f4ae10c7dc2d736c093820218624877bc466aee
Dereck Piché <pichedereck@gmail.com>
2024-03-26 15:00:12 -0400

    updated weird reshapes

--- recurrent_transformer.py
+++ recurrent_transformer.py
@@ -9,6 +9,7 @@ class recurrent_transformer(nn.Module):
             self,
             nb_layers, 
             vocab_size,
+            batch_size,
             token_size,
             hidden_length,
             symbolic_length,
@@ -18,38 +19,44 @@ class recurrent_transformer(nn.Module):
 
         # hypers
         self.gradient_horizon = gradient_horizon
+        self.hidden_length = hidden_length
+        self.batch_size = batch_size
 
         # transformer 
+        self.embedding = nn.Embedding(vocab_size, token_size)
         decoder_layer = nn.TransformerDecoderLayer(d_model=token_size, nhead=4)
         self.transformer = nn.TransformerDecoder(decoder_layer, nb_layers)
 
         # contexts
-        self.hidden_context = torch.rand(hidden_length, token_size) #
-        self.hidden_counts = torch.zeros(hidden_length)
-        self.symbolic_context = torch.zeros(symbolic_length, token_size)
+        self.hidden_context = torch.rand(batch_size, hidden_length, token_size) # (B, S_H, T)
+        self.hidden_counts = torch.zeros(batch_size, hidden_length) #(S)
+        self.symbolic_context = torch.zeros(batch_size, symbolic_length, token_size) # (B, S_S, T)
 
         # dual output
         self.symbolic_out = nn.Linear(token_size*symbolic_length, vocab_size)
         self.hidden_out = nn.Linear(token_size*hidden_length, token_size)
 
-    def autoregress(self, sequence, item, dim):
-        sequence = sequence[:, 1:, :]
-        sequence = torch.cat(sequence, item, dim=dim)
-        return sequence
+    def autoregress(self, sequence, item, dim=1):
+        #print(sequence.shape)
+        #print(item.shape)
 
-    def forward(self, symbol):
+        return torch.cat((sequence[:, 1:], item), dim)
 
-        self.autoregress(self.symbolic_context, symbol, 1)
+    def forward(self, symbols):
 
-        pre_out = self.transformer(self.symbolic_context)
-        s_out, h_out = (self.symbolic_out(pre_out), self.hidden_out(pre_out))
+        symbols = self.embedding(symbols) # (B, 1) -> (B, T)
 
-        self.autoregress(self.hidden_context, h_out, 1) # add tought to sequence
-        self.autoregress(self.hidden_counts, torch.tensor([0]), 0)
+        self.autoregress(self.symbolic_context, symbols, -2) # -> (B, S_S)
+
+        pre_out = self.transformer(self.symbolic_context, self.symbolic_context) # -> (B, S_S, T)
+        s_out, h_out = (self.symbolic_out(pre_out.flatten(start_dim=1)), self.hidden_out(pre_out.flatten(start_dim=1)))
+
+        self.autoregress(self.hidden_context, h_out.unsqueeze(1)) # add tought to sequence
+        self.autoregress(self.hidden_counts, torch.zeros(self.batch_size, 1))
 
         # make gradient cuts past the gradient horizons
-        for pos in range(self.hidden_counts.shape[0]):
-            if self.hidden_counts[pos] > self.gradient_horizon:
+        for pos in range(self.hidden_length):
+            if self.hidden_counts[0, pos] > self.gradient_horizon:
                 hidden = self.hidden_context[:, pos, :]
                 hidden = hidden.clone().detach()
                 self.self.hidden_context[:, pos, :]
--- train.py
+++ train.py
@@ -3,36 +3,52 @@ from transformers import GPT2Tokenizer
 from recurrent_transformer import *
 
 
-def train_sequence(model, optimizer, loss_function, train_tokens, epochs, step_freq):
+def train_sequence(model, optimizer, loss_function, train_tokens, sub_sq_length, batch_size, epochs, step_freq):
     model.train()
+
     for epoch in range(epochs):
+
+        # batch the tokens
+        B = batch_size
+        S = train_tokens.shape[0]
+        K = S // sub_sq_length
+
+        train_tokens = train_tokens[:K * sub_sq_length] # cut the extra extra text
+        train_tokens = torch.reshape(train_tokens, shape=[K, -1]) # (K, S)
 
         loss = 0
 
-        for i in range(0, len(train_tokens) - 1):
-            token = torch.tensor(train_tokens[i], dtype=torch.long).unsqueeze(0)
-            next_token = torch.tensor(train_tokens[i + 1], dtype=torch.long).unsqueeze(0)
-            next_token_hat = model(token)
-            loss += loss_function(next_token_hat, next_token)
+        for b in range(0, K//B):
 
-            if i % step_freq == 0:
-                loss.backward()
-                optimizer.step()
-                optimizer.zero_grad()
-                loss = 0
+            batch_tokens = train_tokens[b * B: (b + 1) * B] # (B, S)
+
+            for i in range(sub_sq_length-1):
+                token = torch.tensor(batch_tokens[:, i], dtype=torch.long).unsqueeze(1) # (B, 1)
+                next_token = torch.tensor(batch_tokens[:, i + 1], dtype=torch.long) # (B)
+                next_token_hat = model(token) # (B, vocab_size)
+                print('next_token_hat', next_token_hat.shape) # ()
+                loss += loss_function(next_token_hat, next_token)
+
+                if i % step_freq == 0:
+                    print('loss:', loss)
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    loss = 0
         
 
 # getting data for training 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 with open('data/shakespear.txt', 'r', encoding='utf-8') as file:
     shakespear_corpus = file.read()
-tokens = tokenizer.encode(shakespear_corpus)
+tokens = torch.Tensor(tokenizer.encode(shakespear_corpus))
 print(tokens[:10])
 
 
 # training 
 model = recurrent_transformer(
     nb_layers=2,
+    batch_size=32,
     vocab_size=50257,
     token_size=512,
     symbolic_length=10,
@@ -44,7 +60,7 @@ optimizer = torch.optim.AdamW(model.parameters())
 loss_function = torch.nn.CrossEntropyLoss()
 
 
-train_sequence(model, optimizer, loss_function, tokens, epochs=1, step_freq=100)
+train_sequence(model, optimizer, loss_function, tokens, epochs=1, sub_sq_length=400, batch_size=32, step_freq=100)
 
 
     
