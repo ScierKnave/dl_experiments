2b51b94c286c31b508bc6f765bf6e75f10b89991
Dereck Piché <pichedereck@gmail.com>
2024-03-31 21:14:06 -0400

    very odd bug occuring

--- reccurent_transformer/recurrent_transformer.py
+++ reccurent_transformer/recurrent_transformer.py
@@ -1,99 +1,104 @@
-    import torch.nn as nn
-    import torch
-    device = 'gpu' if torch.cuda.is_available() else 'cpu'
+import torch.nn as nn
+import torch
+device = 'gpu' if torch.cuda.is_available() else 'cpu'
 
 #
-    class recurrent_transformer(nn.Module):
-        def __init__(
-                self,
-                nb_layers, 
-                nb_heads,
-                vocab_size,
-                batch_size,
-                token_size,
-                hidden_length,
-                symbolic_length,
-                gradient_horizon
-        ):
-            super().__init__()
+class recurrent_transformer(nn.Module):
+    def __init__(
+            self,
+            nb_layers, 
+            nb_heads,
+            vocab_size,
+            batch_size,
+            token_size,
+            hidden_length,
+            symbolic_length,
+            gradient_horizon
+    ):
+        super().__init__()
 
-            # hypers
-            self.gradient_horizon = gradient_horizon
-            self.hidden_length = hidden_length
-            self.batch_size = batch_size
+        # set attributes
+        self.nb_layers = nb_layers
+        self.nb_heads = nb_heads
+        self.vocab_size = vocab_size
+        self.batch_size = batch_size
+        self.token_size = token_size
+        self.hidden_length = hidden_length
+        self.symbolic_length = symbolic_length
+        self.gradient_horizon = gradient_horizon
 
-            # transformer 
-            self.embedding = nn.Embedding(vocab_size, token_size)
-            decoder_layer = nn.TransformerDecoderLayer(d_model=token_size, nhead=nb_heads)
-            self.transformer = nn.TransformerDecoder(decoder_layer, nb_layers)
+        # transformer 
+        self.embedding = nn.Embedding(vocab_size, token_size)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=token_size, nhead=nb_heads)
+        self.transformer = nn.TransformerDecoder(decoder_layer, nb_layers)
 
-            # contexts
-            self.adjust_contexts()
+        # contexts
+        self.adjust_contexts(self.batch_size)
 
-            # dual output
-            self.symbolic_out = nn.Linear(token_size*symbolic_length, vocab_size)
-            self.hidden_out = nn.Linear(token_size*symbolic_length, token_size)
+        # dual output
+        self.symbolic_out = nn.Linear(token_size*symbolic_length, vocab_size)
+        self.hidden_out = nn.Linear(token_size*symbolic_length, token_size)
 
-        def autoregress(self, sequence, item):
-            return torch.cat((sequence[:, item.shape[1]:], item))
-        
-        def adjust_contexts(self, batch_size):
-            # resets contexts with appropriate sizes
-            assert(batch_size >= 1)
-            self.hidden_context = torch.rand(batch_size, self.hidden_length, self.token_size) # (B, H, T)
-            self.hidden_counts = torch.zeros(batch_size, self.hidden_length, 1) #(B, C, 1)
-            self.symbolic_context = torch.zeros(batch_size, self.symbolic_length, self.token_size) # (B, S, T)
-            self.batch_size = batch_size
-
-
-        def prompt(self, input_text, tokenizer, output_limit=50):
-            # prompt the model
-
-            # prepare the model for evalution
-            self.eval()
-            if not self.batch_size == 1: self.adjust_contexts(1)
-
-            # condition on initial text
-            symbols = torch.tensor(tokenizer.encode(input_text)).unsqueeze(0) # -> (1, input_len)
-
-            output_text = ""
-
-            for _ in range(output_limit):
-                symbols = torch.argmax(self.forward(symbols)) # (1, 1)
-                output_text += tokenizer.decoder(symbols[0,0].item())
-
-            return output_text
-                    
+    def autoregress(self, sequence, item):
+        return torch.cat((sequence[:, item.shape[1]:], item), dim=1)
+    
+    def adjust_contexts(self, batch_size):
+        # resets contexts with appropriate sizes
+        assert(batch_size >= 1)
+        self.hidden_context = torch.rand(batch_size, self.hidden_length, self.token_size) # (B, H, T)
+        self.hidden_counts = torch.zeros(batch_size, self.hidden_length, 1) #(B, C, 1)
+        self.symbolic_context = torch.zeros(batch_size, self.symbolic_length, self.token_size) # (B, S, T)
+        self.batch_size = batch_size
 
 
-        def forward(self, symbols):
+    def prompt(self, input_text, tokenizer, output_limit=50):
+        # prompt the model
 
-            # embed input
-            symbols = self.embedding(symbols) # (B, L) -> (B, L, T)
+        # prepare the model for evalution
+        self.eval()
+        if not self.batch_size == 1: self.adjust_contexts(1)
 
-            # auto regress on symbols
-            self.symbolic_context = self.autoregress(self.symbolic_context, symbols) # -> (B, S, T)
+        # condition on initial text
+        symbols = torch.tensor(tokenizer.encode(input_text)).unsqueeze(0) # -> (1, input_len)
 
-            # pass through network
-            pre_out = self.transformer(self.symbolic_context, self.symbolic_context) # -> (B, S, T)
-            pre_out = pre_out.flatten(start_dim=1) # (B, S, T) -> (B, ST)
-            s_out = self.symbolic_out(pre_out) # (B, ST) -> (B, S)
-            h_out = self.hidden_out(pre_out).unsqueeze(1) # (B, ST) -> (B, 1, T)
+        output_text = ""
 
-            # autoregress on thoughts
-            self.hidden_context = self.autoregress(self.hidden_context, h_out) # add thought to sequence
+        for _ in range(output_limit):
+            symbols = torch.argmax(self.forward(symbols)) # (1, 1)
+            output_text += tokenizer.decoder(symbols[0,0].item())
 
-            # make gradient cuts past the gradient horizons
-            if self.training:
-                fresh_counter = torch.zeros(self.batch_size, 1).to(device)
-                self.hidden_counts = self.autoregress(self.hidden_counts, item=fresh_counter)
-                for pos in range(self.hidden_length):
-                    if self.hidden_counts[0, pos] > self.gradient_horizon:
-                        hidden = self.hidden_context[:, pos, :] # (B, 1, T)
-                        hidden = hidden.clone().detach() # (B, 1, T)
-                        self.hidden_context[:, pos, :] = hidden
+        return output_text
+                
 
-            # return logit probabilities for each word in vocab
-            return s_out
+
+    def forward(self, symbols):
+
+        # embed input
+        symbols = self.embedding(symbols) # (B, L) -> (B, L, T)
+
+        # auto regress on input symbols
+        self.symbolic_context = self.autoregress(self.symbolic_context, symbols) # -> (B, S, T)
+
+        # pass through network
+        pre_out = self.transformer(self.symbolic_context, self.symbolic_context) # -> (B, S, T)
+        pre_out = pre_out.flatten(start_dim=1) # (B, S, T) -> (B, ST)
+        s_out = self.symbolic_out(pre_out) # (B, ST) -> (B, S)
+        h_out = self.hidden_out(pre_out).unsqueeze(1) # (B, ST) -> (B, 1, T)
+
+        # autoregress on thoughts
+        self.hidden_context = self.autoregress(self.hidden_context, h_out) # add thought to sequence
+
+        # make gradient cuts past the gradient horizons
+        if self.training:
+            fresh_counter = torch.zeros(self.batch_size, 1, 1).to(device)
+            self.hidden_counts = self.autoregress(self.hidden_counts, item=fresh_counter)
+            for pos in range(self.hidden_length):
+                if self.hidden_counts[0, pos] > self.gradient_horizon:
+                    hidden = self.hidden_context[:, pos, :] # (B, 1, T)
+                    hidden = hidden.clone().detach() # (B, 1, T)
+                    self.hidden_context[:, pos, :] = hidden
+
+        # return logit probabilities for each word in vocab
+        return s_out
 
 
--- reccurent_transformer/train.py
+++ reccurent_transformer/train.py
@@ -32,34 +32,48 @@ def train_sequence(model,
         loss = 0
 
         for b in range(0, K//B):
+            
+            # ensure gradients are cleared
+            optimizer.zero_grad()   
+            loss = 0
 
             batch_tokens = train_tokens[b * B: (b + 1) * B] # (B, S)
 
             for i in range(sub_sq_length-1):
-                token = torch.tensor(batch_tokens[:, i], dtype=torch.long).unsqueeze(1) # (B, 1)
-                next_token = torch.tensor(batch_tokens[:, i + 1], dtype=torch.long) # (B)
-                next_token_hat = model(token) # (B, vocab_size)
-                #print('next_token_hat', next_token_hat.shape) # ()
-                loss += loss_function(next_token_hat, next_token)
 
-                if i % step_freq == 0:
+                
+                # get tokens
+                tokens = torch.tensor(batch_tokens[:, i], dtype=torch.long).unsqueeze(1) # (B, 1)
+                next_tokens = torch.tensor(batch_tokens[:, i + 1], dtype=torch.long) # (B)
+
+                # forward pass
+                next_tokens_hat = model(tokens) # (B, vocab_size) unormalized probs
+
+                # add to error to loss
+                loss += loss_function(next_tokens_hat, next_tokens)
+
+                if (i+1) % step_freq == 0:
                     print('loss:', loss)
                     loss.backward()
                     optimizer.step()
                     grad_step_count+=1
-                    optimizer.zero_grad()
                     loss = 0
+                    optimizer.zero_grad()
+                    model.zero_grad()
                     print("Training on batch...")
 
-                if grad_step_count % model_save_freq == 0:
+                if (grad_step_count+1) % model_save_freq == 0:
                     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
                     save_path = f"model_{current_time}_step_{grad_step_count}.pt"
                     torch.save(model, save_path)
                     print(f"Saved model at step {grad_step_count} to {save_path}")
 
+       
+
 
 # import configuration
-with open('config.yaml', 'r') as file:config = yaml.safe_load(file)
+with open('reccurent_transformer/config_small.yaml', 'r') as file: 
+    config = yaml.safe_load(file)
 
 # getting data for training 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
@@ -72,7 +86,11 @@ model = recurrent_transformer(**config['model'])
 # train the model
 optimizer = torch.optim.AdamW(model.parameters())
 loss_function = torch.nn.CrossEntropyLoss()
-train_sequence(**config['training'])
+train_sequence(**config['training'], 
+               model=model, 
+               optimizer=optimizer, 
+               loss_function=loss_function,
+               train_tokens=tokens)
 
 
     
